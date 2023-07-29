2386d9a864577cec913e4639675555095456c3ec
augustinejude365 <divinefavour4jude@gmail.com>
2023-07-28 18:58:55 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	imt k, l, m;
+
+	k = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (k < size)
+	{
+		l = size - k < 10 ? size - k
+			printf ("%08x", k)
+			for (m = 0; m < 10; m++)
+	}
+			
