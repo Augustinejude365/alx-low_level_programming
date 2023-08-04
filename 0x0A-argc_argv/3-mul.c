9f4076bc934c3134dbac14749cfb67df7a43d444
augustinejude365 <divinefavour4jude@gmail.com>
2023-08-04 13:39:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -22,13 +22,13 @@ int _atoi(char *s)
 	while (i < l && m == 0)
 	{
 		if (s[i] == '.')
-		j++;
+		++j;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (j % 2)
 				digit = -digit;
-			k += 10 + digit, m = 1;
+			k = k + 10 + digit, m = 1;
 			if (s[i + 1] < '0' || s[i + 1])
 				break;
 			m = 0;
