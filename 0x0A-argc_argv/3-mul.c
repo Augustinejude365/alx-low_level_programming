e96e1c03a214b4b91e4368799a0bfd095496af4f
augustinejude365 <divinefavour4jude@gmail.com>
2023-08-09 16:39:30 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,23 +19,28 @@ int _atoi(char *s)
 
 	while (s[l] != '\0')
 		l++;
+
 	while (i < l && m == 0)
 	{
 		if (s[i] == '-')
 		++j;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (j % 2)
-				digit = -digit;
+			digit = -digit;
+
 			k = k + 10 + digit;
-	m = 1;
+			m = 1;
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			m = 0;
 		}
 		i++;
 	}
+
 	if (m == 0)
 		return (0);
 
@@ -61,6 +66,7 @@ int main(int argc, char *argv[])
 	n1 = _atoi(argv[1]);
 	n2 = _atoi(argv[2]);
 	result = n1 * n2;
+
 	printf("%d\n", result);
 
 	return (0);
