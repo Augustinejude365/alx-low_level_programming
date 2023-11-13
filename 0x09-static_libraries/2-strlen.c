16c66220f0b76cfd5ca7a5b4bbc7cbbd079adece
Augustinejude365 <divinefavour4jude@gmail.com>
2023-11-13 09:35:04 -0800

    static lib

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
- * _strien - returns the lenght of a string
+ * _strlen - returns the lenght of a string
  * @s: string
  * JA codes
  * Return: lenght
  */
-int _strien(char *s)
+int _strlen(char *s)
 {
 	int longk = 0;
-
 	while (*s != '\0')
 	{
 		longk++;
