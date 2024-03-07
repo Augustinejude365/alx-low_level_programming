1a886fec72c00c78ac39ab965619104ad0ac00fd
Augustinejude365 <divinefavour4jude@gmail.com>
2024-03-07 15:54:06 -0800

    Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'Print all possible different combinations of three digits
- * Return: Always 0
+ * main - A function that prints all possible different combinations of three digits
+ * followed by a comma space and a new line at the end.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m, l;
+	int i, j, k;
 
-	for (n = 48; n < 58; n++)
+	for (i = 48; i < 58; i++)
 	{
-		for (m = 49; m < 58; m++)
+		for (j = 49; j < 58; j++)
 		{
-		for (l = 50; l < 58; l++)
-		{
-		if (l > m && m > n)
-		{
-		putchar(n);
-	putchar(m);
-if (n ! = 55 || m ! = 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+			for (k = 50; k < 58; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
