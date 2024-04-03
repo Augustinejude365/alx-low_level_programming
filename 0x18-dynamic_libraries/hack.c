175b089060ecd83206dce183171a056dd07c6b55
Augustinejude365 <divinefavour4jude@gmail.com>
2024-04-03 09:57:20 -0700

    Without libraries what have we? We have no past and no future

--- 0x18-dynamic_libraries/hack.c
+++ 0x18-dynamic_libraries/hack.c
@@ -2,14 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * printf - a function that prints output to the stdio.
- * @format: the output format
- * Return: Nothing
- */
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 + 9\n",  17);
-	write(1, "Congratulations, you won the Jackpot!\n", 38);
+	write(1, "9 8 10 24 75 +9\n",  17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
